93ec27da8908bc49c822a902401a1ff39bb96783
Bence <micklayvince@gmail.com>
2024-07-26 19:05:09 +0800

    back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [lightMode, setLightMode] = useState(false);
 
   return (
-    <div className="bg-custom min-h-screen w-full">
+    <div className="md:bg-custom bg-custom-space bg-cover bg-fixed ">
       <Navbar lightMode={lightMode} setLightMode={setLightMode} />
       <LandingPage />
       <Hero lightMode={lightMode} setLightMode={setLightMode} />
