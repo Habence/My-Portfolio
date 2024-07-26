273d14969138e227d743859234e624255357b7be
Bence <micklayvince@gmail.com>
2024-07-27 04:56:40 +0800

    eh

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useRef } from "react";
-import {
-  motion,
-  useInView,
-  useAnimation,
-  useTransform,
-  useScroll,
-} from "framer-motion";
-
 import "../index.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div className="text-white px-20 py-[10rem]">
+      <Parallax pages={4}>
+        <ParallaxLayer speed={1}>
+          <p className="text-5xl text-white">HEllO WELCOME</p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+          <p className="text-5xl text-white uppercase">
+            Web Development is fun
+          </p>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
 };
 
 export default LandingPage;
