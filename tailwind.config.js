1822b137204d7b431c8d9e8a4651d066c6f94824
Bence <micklayvince@gmail.com>
2024-07-26 20:09:07 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       screens: {
         xs: { max: "500px" }, // Custom breakpoint for screens below 500px
         max_sm: { max: "640px" }, // Custom breakpoint for screens below 500px
+        max_md: { max: "748px" },
       },
       backdropBlur: {
         xs: "2px",
