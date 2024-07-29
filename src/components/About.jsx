6dc9efa4b4ecddd0d767c0f4256e919ae696ec6b
Bence <micklayvince@gmail.com>
2024-07-30 02:00:31 +0800

    back

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,20 +3,10 @@ import Zoom from "@mui/material/Zoom";
 import Slide from "@mui/material/Slide";
 import AOS from "aos";
 import { MdArrowOutward } from "react-icons/md";
-import { motion } from "framer-motion";
-import skillsImages from "../assets/skillsImages";
 
 const About = ({ lightMode }) => {
   useEffect(() => {
     AOS.init({ duration: 2000 });
-  }, []);
-
-  const [width, setwidth] = useState(0);
-  const carousel = useRef();
-
-  useEffect(() => {
-    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
-    setwidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
   }, []);
 
   return (
@@ -59,61 +49,41 @@ const About = ({ lightMode }) => {
         ></div>
       </Slide>
 
-      <motion.div className="carousel cursor-grab  overflow-hidden ">
-        <motion.div
-          drag="x"
-          ref={carousel}
-          dragConstraints={{ right: 0, left: -width }}
-          className="flex"
-        >
-          <div
-            data-aos="fade-right"
-            className={
-              lightMode
-                ? "mt-10 inner-carousel text-black font-poppins font-normal text-xs md:pr-[10rem] leading-relaxed lg:text-base lg:pr-[15rem] 2xl:pr-[20rem] 2xl:text-lg"
-                : "mt-10 inner-carousel text-white rounded-lg font-poppins font-normal text-xs md:pr-[10rem] leading-relaxed lg:text-base lg:pr-[15rem] 2xl:pr-[20rem] 2xl:text-lg"
-            }
-          >
-            <Zoom in={true} timeout={1000}>
-              <p className="">
-                As a seasoned Web Developer based in Baguio City, Philippines, I
-                hold a Bachelor of Science in Information Technology (BSIT) from
-                the University of the Cordilleras. I specialize in crafting
-                dynamic and responsive websites using JavaScript, HTML, CSS, and
-                ReactJS. My expertise extends to mobile app development with
-                Java and Android Studio, where I collaboratively implemented a
-                QR code transaction system for a recycling app as part of our
-                capstone project. Additionally, my internship as a Junior
-                Backend Developer at BitShares Labs Inc. honed my skills in
-                enhancing user interactions through a Discord chatbot powered by
-                neural networks.
-              </p>
-            </Zoom>
-            <br />
-            <Zoom in={true} timeout={1000}>
-              <p>
-                I am committed to continuous learning and professional growth,
-                having attended various seminars and courses to expand my skill
-                set. Proficient in version control with Git and database
-                management with MySQL, I am eager to apply my foundational
-                programming skills in an entry-level role. My goal is to
-                contribute to meaningful projects and collaborate with
-                experienced professionals to drive digital transformation and
-                innovative solutions.
-              </p>
-            </Zoom>
-          </div>
-          <div className="flex">
-            {skillsImages.map((image) => {
-              return (
-                <motion.div key={image} className="h-36 min-w-[50%]">
-                  <img src={image} alt="" />
-                </motion.div>
-              );
-            })}
-          </div>
-        </motion.div>
-      </motion.div>
+      <div
+        data-aos="fade-right"
+        className={
+          lightMode
+            ? "mt-10 inner-carousel text-black font-poppins font-normal text-xs md:pr-[10rem] leading-relaxed lg:text-base lg:pr-[15rem] 2xl:pr-[20rem] 2xl:text-lg"
+            : "mt-10 inner-carousel text-white rounded-lg font-poppins font-normal text-xs md:pr-[10rem] leading-relaxed lg:text-base lg:pr-[15rem] 2xl:pr-[20rem] 2xl:text-lg"
+        }
+      >
+        <Zoom in={true} timeout={1000}>
+          <p>
+            As a seasoned Web Developer based in Baguio City, Philippines, I
+            hold a Bachelor of Science in Information Technology (BSIT) from the
+            University of the Cordilleras. I specialize in crafting dynamic and
+            responsive websites using JavaScript, HTML, CSS, and ReactJS. My
+            expertise extends to mobile app development with Java and Android
+            Studio, where I collaboratively implemented a QR code transaction
+            system for a recycling app as part of our capstone project.
+            Additionally, my internship as a Junior Backend Developer at
+            BitShares Labs Inc. honed my skills in enhancing user interactions
+            through a Discord chatbot powered by neural networks.
+          </p>
+        </Zoom>
+        <br />
+        <Zoom in={true} timeout={1000}>
+          <p>
+            I am committed to continuous learning and professional growth,
+            having attended various seminars and courses to expand my skill set.
+            Proficient in version control with Git and database management with
+            MySQL, I am eager to apply my foundational programming skills in an
+            entry-level role. My goal is to contribute to meaningful projects
+            and collaborate with experienced professionals to drive digital
+            transformation and innovative solutions.
+          </p>
+        </Zoom>
+      </div>
 
       <a
         href="/RESUME - Micklay_Vince.pdf"
