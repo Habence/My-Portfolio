e3e2a91d97ef28b20eda8650b9d3c16c6bc0e519
Bence <micklayvince@gmail.com>
2024-08-03 02:09:49 +0800

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,32 +48,28 @@ const Navbar = ({ lightMode, setLightMode }) => {
 
   return (
     <div
-      className={`fixed w-full  z-50 ${
+      className={`fixed w-full z-50 ${
         visible
           ? "translate-y-0 transition-transform duration-300"
-          : " transition-transform duration-300 -translate-y-full"
+          : "transition-transform duration-300 -translate-y-full"
       } ${
         lightMode
           ? "bg-[#FFFFF0] transition-colors duration-500"
-          : "backdrop-blur-sm bg-black bg-opacity-40 bg-blur  transition-colors duration-500"
+          : "backdrop-blur-sm bg-black bg-opacity-40 transition-colors duration-500"
       } ${lightMode ? "text-black" : "text-white"}`}
     >
       <div
-        className={
-          lightMode
-            ? "flex flex-row px-10 py-5 justify-between items-center relative cursor-default xs:px-5 max-sm:px-20 md:px-20 lg:px-28 xl:px-40 2xl:px-54"
-            : "flex flex-row px-10 py-5 justify-between items-center relative cursor-default xs:px-5 max-sm:px-20 md:px-20 lg:px-28 xl:px-40 2xl:px-54"
-        }
+        className={`flex flex-row px-5 py-5 justify-between items-center relative cursor-default ${
+          lightMode ? "text-black" : "text-white"
+        } xs:px-5 sm:px-10 md:px-20 lg:px-28 xl:px-38 2xl:px-54`}
       >
         <Slide in={true} direction="right" timeout={1000}>
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <a
               href="#home"
-              className={
-                lightMode
-                  ? "text-black font-poppins font-semibold text-xl"
-                  : "text-white font-poppins font-semibold text-xl"
-              }
+              className={`font-poppins font-semibold text-xl ${
+                lightMode ? "text-black" : "text-white"
+              }`}
             >
               <span className={lightMode ? "text-teal-600" : "text-[#FEC45D]"}>
                 Vince
@@ -82,20 +78,15 @@ const Navbar = ({ lightMode, setLightMode }) => {
             </a>
             <LuSun
               size={30}
-              className={
-                lightMode
-                  ? "mt-[0.10rem] cursor-pointer text-teal-600 hover:scale-110 duration-300"
-                  : "mt-[0.10rem] cursor-pointer text-[#FEC45D] hover:scale-110 duration-300"
-              }
+              className={`mt-[0.10rem] cursor-pointer ${
+                lightMode ? "text-teal-600" : "text-[#FEC45D]"
+              } hover:scale-110 duration-300`}
               onClick={() => setLightMode(!lightMode)}
             />
           </div>
         </Slide>
         <Slide in={true} direction="left" timeout={1000}>
-          <div
-            onClick={handleNav}
-            className="cursor-pointer z-50 lg:flex xl:hidden"
-          >
+          <div onClick={handleNav} className="cursor-pointer z-50 lg:hidden">
             {nav ? (
               <IoIosClose
                 size={30}
@@ -111,130 +102,47 @@ const Navbar = ({ lightMode, setLightMode }) => {
         </Slide>
         <Slide in={true} direction="left" timeout={1000}>
           <ul
-            className={
-              lightMode
-                ? "flex flex-row gap-10 font-poppins font-semibold text-black hidden xl:flex"
-                : "flex flex-row gap-10 font-poppins font-semibold text-white hidden xl:flex"
-            }
+            className={`hidden xl:flex flex-row gap-10 font-poppins font-semibold ${
+              lightMode ? "text-black" : "text-white"
+            }`}
           >
-            <li
-              className={
-                lightMode
-                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
-              }
-            >
-              <a href="#about">About</a>
-              <span
-                className={
-                  lightMode
-                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                }
-              ></span>
-            </li>
-            <li
-              className={
-                lightMode
-                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
-              }
-            >
-              <a href="#skill">Skills</a>
-              <span
-                className={
-                  lightMode
-                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                }
-              ></span>
-            </li>
-
-            <li
-              className={
-                lightMode
-                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
-              }
-            >
-              <a href="#experience">Experience</a>
-              <span
-                className={
-                  lightMode
-                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                }
-              ></span>
-            </li>
-
-            <li
-              className={
-                lightMode
-                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
-              }
-            >
-              <a href="#project">Projects</a>
-              <span
-                className={
-                  lightMode
-                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                }
-              ></span>
-            </li>
-
-            <li
-              className={
-                lightMode
-                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
-              }
-            >
-              <a href="#education">Education</a>
-              <span
-                className={
-                  lightMode
-                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                }
-              ></span>
-            </li>
-
-            <li
-              className={
-                lightMode
-                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
-              }
-            >
-              <a href="#contact">Contact</a>
-              <span
-                className={
-                  lightMode
-                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                }
-              ></span>
-            </li>
+            {[
+              { name: "About", href: "#about" },
+              { name: "Skills", href: "#skill" },
+              { name: "Experience", href: "#experience" },
+              { name: "Projects", href: "#project" },
+              { name: "Education", href: "#education" },
+              { name: "Contact", href: "#contact" },
+            ].map((item) => (
+              <li
+                key={item.name}
+                className={`relative group hover:scale-110 duration-300 ${
+                  lightMode ? "hover:text-teal-600" : "hover:text-[#FEC45D]"
+                }`}
+              >
+                <a href={item.href}>{item.name}</a>
+                <span
+                  className={`block mx-auto mt-1 w-0 h-0.5 ${
+                    lightMode ? "bg-teal-600" : "bg-[#FEC45D]"
+                  } transition-all duration-300 group-hover:w-full`}
+                ></span>
+              </li>
+            ))}
           </ul>
         </Slide>
 
         {nav && (
           <div
-            className={
-              lightMode
-                ? "fixed inset-0 duration-300 overflow-y-auto bg-[#FFFFF0] h-[100vh] text-black flex flex-col items-center justify-center z-40 xl:hidden"
-                : "fixed inset-0 duration-300 overflow-y-auto bg-black h-[100vh] text-white flex flex-col items-center justify-center z-40 xl:hidden"
-            }
+            className={`fixed inset-0 duration-300 overflow-y-auto ${
+              lightMode ? "bg-[#FFFFF0] text-black" : "bg-black text-white"
+            } h-full flex flex-col items-center justify-center z-40 xl:hidden`}
           >
             <Slide in={true} direction="up" timeout={1000}>
-              <div className="absolute top-[9rem] max_sm:-left-2 sm:left-32 lg:left-[19rem]">
-                <p className="text-white font-bold text-xl tracking-widest -rotate-90">
+              <div className="absolute top-24 left-10">
+                <p className="font-bold text-xl tracking-widest -rotate-90">
                   <span
                     className={lightMode ? "text-teal-600" : "text-[#FEC45D]"}
                   >
-                    {" "}
                     MENU
                   </span>
                 </p>
@@ -244,81 +152,35 @@ const Navbar = ({ lightMode, setLightMode }) => {
             <div className="flex flex-col">
               <Slide in={true} timeout={1000}>
                 <ul className="space-y-10 text-3xl font-bold">
-                  <Slide in={true} direction="right" timeout={1000}>
-                    <li>
-                      <a
-                        href="#about"
-                        className="cursor-pointer"
-                        onClick={handleNav}
-                      >
-                        ABOUT
-                      </a>
-                    </li>
-                  </Slide>
-
-                  <Slide in={true} direction="left" timeout={1000}>
-                    <li>
-                      <a
-                        href="#skill"
-                        className="cursor-pointer"
-                        onClick={handleNav}
-                      >
-                        SKILLS
-                      </a>
-                    </li>
-                  </Slide>
-
-                  <Slide in={true} direction="right" timeout={1000}>
-                    <li>
-                      <a
-                        href="#experience"
-                        className="cursor-pointer"
-                        onClick={handleNav}
-                      >
-                        EXPERIENCE
-                      </a>
-                    </li>
-                  </Slide>
-
-                  <Slide in={true} direction="left" timeout={1000}>
-                    <li>
-                      <a
-                        href="#project"
-                        className="cursor-pointer"
-                        onClick={handleNav}
-                      >
-                        PROJECTS
-                      </a>
-                    </li>
-                  </Slide>
-
-                  <Slide in={true} direction="right" timeout={1000}>
-                    <li>
-                      <a
-                        href="#education"
-                        className="cursor-pointer"
-                        onClick={handleNav}
-                      >
-                        EDUCATION
-                      </a>
-                    </li>
-                  </Slide>
-
-                  <Slide in={true} direction="left" timeout={1000}>
-                    <li>
-                      <a
-                        href="#contact"
-                        className="cursor-pointer"
-                        onClick={handleNav}
-                      >
-                        CONTACT
-                      </a>
-                    </li>
-                  </Slide>
+                  {[
+                    { name: "ABOUT", href: "#about" },
+                    { name: "SKILLS", href: "#skill" },
+                    { name: "EXPERIENCE", href: "#experience" },
+                    { name: "PROJECTS", href: "#project" },
+                    { name: "EDUCATION", href: "#education" },
+                    { name: "CONTACT", href: "#contact" },
+                  ].map((item, index) => (
+                    <Slide
+                      key={item.name}
+                      in={true}
+                      direction={index % 2 === 0 ? "right" : "left"}
+                      timeout={1000}
+                    >
+                      <li>
+                        <a
+                          href={item.href}
+                          className="cursor-pointer"
+                          onClick={handleNav}
+                        >
+                          {item.name}
+                        </a>
+                      </li>
+                    </Slide>
+                  ))}
                 </ul>
               </Slide>
             </div>
-            <div className="flex mt-[2rem] flex-col gap-4">
+            <div className="flex mt-8 flex-col gap-4">
               <Slide in={true} direction="right" timeout={1000}>
                 <p className="font-bold">micklayvince@gmail.com</p>
               </Slide>
