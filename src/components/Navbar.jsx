6800621879a193afe16f01f8b4a7b4a70cf04012
Bence <micklayvince@gmail.com>
2024-08-02 08:02:25 +0800

    sd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,34 +32,37 @@ const Navbar = ({ lightMode, setLightMode }) => {
   }, [nav]);
 
   useEffect(() => {
+    // Function to handle scroll events
     const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      const visible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+      const currentScrollPos = window.pageYOffset; // Get the current scroll position
+      const visible = prevScrollPos > currentScrollPos || currentScrollPos < 10; // Determine if the navbar should be visible
 
-      setPrevScrollPos(currentScrollPos);
-      setVisible(visible);
+      setPrevScrollPos(currentScrollPos); // Update the previous scroll position
+      setVisible(visible); // Update the visibility state
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll); // Add scroll event listener
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); // Clean up the event listener
   }, [prevScrollPos]);
 
   return (
     <div
-      className={`fixed w-full z-50 ${
+      className={`fixed w-full  z-50 ${
         visible
           ? "translate-y-0 transition-transform duration-300"
-          : "transition-transform duration-300 -translate-y-full"
+          : " transition-transform duration-300 -translate-y-full"
       } ${
         lightMode
           ? "bg-[#FFFFF0] transition-colors duration-500"
-          : "backdrop-blur-sm bg-black bg-opacity-40 transition-colors duration-500"
+          : "backdrop-blur-sm bg-black bg-opacity-40 bg-blur  transition-colors duration-500"
       } ${lightMode ? "text-black" : "text-white"}`}
     >
       <div
         className={
-          "flex flex-row px-10 py-5 justify-between items-center relative cursor-default xs:px-5 md:px-20 lg:px-28 xl:px-38 2xl:px-54"
+          lightMode
+            ? "flex flex-row px-10 py-5 justify-between items-center relative cursor-default xs:px-5 max-sm:px-20 md:px-20 lg:px-28 xl:px-38 2xl:px-54"
+            : "flex flex-row px-10 py-5 justify-between items-center relative cursor-default xs:px-5 max-sm:px-20 md:px-20 lg:px-28 xl:px-38 2xl:px-54"
         }
       >
         <Slide in={true} direction="right" timeout={1000}>
@@ -89,7 +92,10 @@ const Navbar = ({ lightMode, setLightMode }) => {
           </div>
         </Slide>
         <Slide in={true} direction="left" timeout={1000}>
-          <div onClick={handleNav} className="cursor-pointer z-50 lg:hidden">
+          <div
+            onClick={handleNav}
+            className="cursor-pointer z-50 lg:flex xl:hidden"
+          >
             {nav ? (
               <IoIosClose
                 size={30}
@@ -107,45 +113,118 @@ const Navbar = ({ lightMode, setLightMode }) => {
           <ul
             className={
               lightMode
-                ? "hidden lg:flex flex-row gap-10 font-poppins font-semibold text-black"
-                : "hidden lg:flex flex-row gap-10 font-poppins font-semibold text-white"
+                ? "flex flex-row gap-10 font-poppins font-semibold text-black hidden xl:flex"
+                : "flex flex-row gap-10 font-poppins font-semibold text-white hidden xl:flex"
             }
           >
-            {[
-              "About",
-              "Skills",
-              "Experience",
-              "Projects",
-              "Education",
-              "Contact",
-            ].map((item, index) => (
-              <li
-                key={index}
+            <li
+              className={
+                lightMode
+                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
+                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+              }
+            >
+              <a href="#about">About</a>
+              <span
                 className={
                   lightMode
-                    ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
-                    : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
+                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
                 }
-              >
-                <a href={`#${item.toLowerCase()}`}>{item}</a>
-                <span
-                  className={
-                    lightMode
-                      ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
-                      : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
-                  }
-                ></span>
-              </li>
-            ))}
+              ></span>
+            </li>
+            <li
+              className={
+                lightMode
+                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
+                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+              }
+            >
+              <a href="#skill">Skills</a>
+              <span
+                className={
+                  lightMode
+                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
+                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
+                }
+              ></span>
+            </li>
+
+            <li
+              className={
+                lightMode
+                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
+                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+              }
+            >
+              <a href="#experience">Experience</a>
+              <span
+                className={
+                  lightMode
+                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
+                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
+                }
+              ></span>
+            </li>
+
+            <li
+              className={
+                lightMode
+                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
+                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+              }
+            >
+              <a href="#project">Projects</a>
+              <span
+                className={
+                  lightMode
+                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
+                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
+                }
+              ></span>
+            </li>
+
+            <li
+              className={
+                lightMode
+                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
+                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+              }
+            >
+              <a href="#education">Education</a>
+              <span
+                className={
+                  lightMode
+                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
+                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
+                }
+              ></span>
+            </li>
+
+            <li
+              className={
+                lightMode
+                  ? "relative group hover:scale-110 duration-300 hover:text-teal-600"
+                  : "relative group hover:scale-110 duration-300 hover:text-[#FEC45D]"
+              }
+            >
+              <a href="#contact">Contact</a>
+              <span
+                className={
+                  lightMode
+                    ? "block mx-auto mt-1 w-0 h-0.5 bg-teal-600 transition-all duration-300 group-hover:w-full"
+                    : "block mx-auto mt-1 w-0 h-0.5 bg-[#FEC45D] transition-all duration-300 group-hover:w-full"
+                }
+              ></span>
+            </li>
           </ul>
         </Slide>
-
         {nav && (
           <div
             className={
               lightMode
-                ? "fixed inset-0 duration-300 overflow-y-auto bg-[#FFFFF0] h-[100vh] text-black flex flex-col items-center justify-center z-40 lg:hidden"
-                : "fixed inset-0 duration-300 overflow-y-auto bg-black h-[100vh] text-white flex flex-col items-center justify-center z-40 lg:hidden"
+                ? "fixed inset-0 duration-300 overflow-y-auto bg-[#FFFFF0] h-[100vh] text-black flex flex-col items-center justify-center z-40 xl:hidden"
+                : "fixed inset-0 duration-300 overflow-y-auto bg-black h-[100vh] text-white flex flex-col items-center justify-center z-40 xl:hidden"
             }
           >
             <Slide in={true} direction="up" timeout={1000}>
@@ -164,31 +243,77 @@ const Navbar = ({ lightMode, setLightMode }) => {
             <div className="flex flex-col">
               <Slide in={true} timeout={1000}>
                 <ul className="space-y-10 text-3xl font-bold">
-                  {[
-                    "About",
-                    "Skills",
-                    "Experience",
-                    "Projects",
-                    "Education",
-                    "Contact",
-                  ].map((item, index) => (
-                    <Slide
-                      key={index}
-                      in={true}
-                      direction={index % 2 === 0 ? "right" : "left"}
-                      timeout={1000}
-                    >
-                      <li>
-                        <a
-                          href={`#${item.toLowerCase()}`}
-                          className="cursor-pointer"
-                          onClick={handleNav}
-                        >
-                          {item.toUpperCase()}
-                        </a>
-                      </li>
-                    </Slide>
-                  ))}
+                  <Slide in={true} direction="right" timeout={1000}>
+                    <li>
+                      <a
+                        href="#about"
+                        className="cursor-pointer"
+                        onClick={handleNav}
+                      >
+                        ABOUT
+                      </a>
+                    </li>
+                  </Slide>
+
+                  <Slide in={true} direction="left" timeout={1000}>
+                    <li>
+                      <a
+                        href="#skill"
+                        className="cursor-pointer"
+                        onClick={handleNav}
+                      >
+                        SKILLS
+                      </a>
+                    </li>
+                  </Slide>
+
+                  <Slide in={true} direction="right" timeout={1000}>
+                    <li>
+                      <a
+                        href="#experience"
+                        className="cursor-pointer"
+                        onClick={handleNav}
+                      >
+                        EXPERIENCE
+                      </a>
+                    </li>
+                  </Slide>
+
+                  <Slide in={true} direction="left" timeout={1000}>
+                    <li>
+                      <a
+                        href="#project"
+                        className="cursor-pointer"
+                        onClick={handleNav}
+                      >
+                        PROJECTS
+                      </a>
+                    </li>
+                  </Slide>
+
+                  <Slide in={true} direction="right" timeout={1000}>
+                    <li>
+                      <a
+                        href="#education"
+                        className="cursor-pointer"
+                        onClick={handleNav}
+                      >
+                        EDUCATION
+                      </a>
+                    </li>
+                  </Slide>
+
+                  <Slide in={true} direction="left" timeout={1000}>
+                    <li>
+                      <a
+                        href="#contact"
+                        className="cursor-pointer"
+                        onClick={handleNav}
+                      >
+                        CONTACT
+                      </a>
+                    </li>
+                  </Slide>
                 </ul>
               </Slide>
             </div>
